7d7ea39f272f9db83696b542f48b86f32240f782
CaptainDespair <danila.syrokvasov@yandex.ru>
2023-03-22 14:22:40 +0300

    don't understand

--- app.py
+++ app.py
@@ -1,48 +1,65 @@
 from flask import *
-from flask_sqlalchemy import *
+from flask_sqlalchemy import SQLAlchemy
 
 import pandas as pd
 import os
 
 UPLOAD_FOLDER = os.path.join('csv_files')
- 
-ALLOWED_EXTENSIONS = {'csv'}
-        
-app = Flask(__name__)
 
+ALLOWED_EXTENSIONS = {'csv'}
+
+app = Flask(__name__)
 app.secret_key = 'secret123'
 
 app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+app.config['SQLALCHEMY_DATABASE_URI']='sqlite:///datasets.db'
+
 db = SQLAlchemy(app)
 
-class Dataset(db.Model):
+class DataSet(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(80), unique=True, nullable=False)
+    name = db.Column(db.String(100), nullable=False)
+
 
 @app.route('/', methods=['GET', 'POST'])
 def upload():
-    session['uploaded_files'] = ''
     if request.method == 'POST':
+        names = []
         files = request.files.getlist("file")
-        file = Dataset.query.filter_by(id).first_or_404()
-        for file in files:
+        for file in files: 
             file.save(os.path.join(app.config['UPLOAD_FOLDER'], file.filename))
-            session['uploaded_files'] += os.path.join(app.config['UPLOAD_FOLDER'], file.filename+' ')
-        return render_template('success.html', files=files)
-    return render_template("index.html")
+            # session['uploaded_files'] += os.path.join(app.config['UPLOAD_FOLDER'], file.filename+' ')
+            names.append(file.filename)
+        try:
+            for name in names:
+                dataset = DataSet(name=name)
+                if not db.session.query(db.session.query(DataSet).filter_by(name=name).exists()).scalar():
+                    db.session.add(dataset)
+                    db.session.commit()
+            return render_template('success.html', files=files)
+        except (KeyboardInterrupt):
+            return 'Ошибка'
+    else:
+        return render_template('index.html')
+    
+#     # session['uploaded_files'] = ''
+#     # if request.method == 'POST':
+#     #     files = request.files.getlist("file")
+#     #     for file in files:
+#     #         file.save(os.path.join(app.config['UPLOAD_FOLDER'], file.filename))
+#     #         session['uploaded_files'] += os.path.join(app.config['UPLOAD_FOLDER'], file.filename+' ')
+#     #     return render_template('success.html', files=files)
+    #return render_template("index.html")
  
- 
-@app.route('/show')
+@app.route('/show/<string:name>')
 def show():
-    htmls = []
+    datasets = DataSet.query.all()
+
     data_session = session.get('uploaded_files')
-    files = data_session.split(' ')[0:-1]
-    for file in files:
-        filedir = pd.read_csv(file, error_bad_lines=False, engine="python", encoding='unicode_escape')
-        file_to_html = filedir.to_html()
-        htmls.append(file_to_html) 
-    return render_template('read.html', htmls=htmls)
- 
- 
+    file = (os.path.join(app.config['UPLOAD_FOLDER'], db.session.query(DataSet).filter_by(id=id)))
+    filedir = pd.read_csv(file, error_bad_lines=False, engine="python", encoding='unicode_escape')
+    file_to_html = filedir.to_html()
+    return render_template('read.html', datasets=datasets)
+
 if __name__ == '__main__':
     app.run(debug=True)
