7d1f33352439ae3c3f0728f2a957ec7d697f36f7
CaptainDespair <danila.syrokvasov@yandex.ru>
2023-03-23 16:26:33 +0300

    LIST+, DELETE+, UPLOAD+, VIEW+

--- app.py
+++ app.py
@@ -4,55 +4,64 @@ from flask_sqlalchemy import SQLAlchemy
 import pandas as pd
 import os
 
+#--Определяем папку хранения csv-файлов и расширение для загрузки
 UPLOAD_FOLDER = os.path.join('csv_files')
-
 ALLOWED_EXTENSIONS = {'csv'}
 
+#--Конфиги, подключение к SQLite
 app = Flask(__name__)
 app.secret_key = 'secret123'
-
 app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 app.config['SQLALCHEMY_DATABASE_URI']='sqlite:///datasets.db'
 
+#--Определяем ОРМ как SQLAlchemy 
 db = SQLAlchemy(app)
 
+#--Создаем "модель" для хранения имён csv-файлов
 class DataSet(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(100), nullable=False)
 
 
+#--Отображение/удаление датасетов из БД 
 @app.route('/', methods=['GET', 'POST'])
-def upload():
-    if request.method == 'POST':
-        names = []
-        files = request.files.getlist("file")
+def delete():
+    try:
         datasets = DataSet.query.all()
-        for file in files: 
-            file.save(os.path.join(app.config['UPLOAD_FOLDER'], file.filename))
-            # session['uploaded_files'] += os.path.join(app.config['UPLOAD_FOLDER'], file.filename+' ')
-            names.append(file.filename)
-        try:
+        if request.method == 'POST':
+            db.session.query(DataSet).delete()
+            db.session.commit()
+            return render_template('index.html')
+    except:
+        return render_template('index.html', datasets=datasets)    
+    else:   
+        return render_template('index.html', datasets=datasets)
+    
+#--Загрузка датасетов в БД
+@app.route('/upload', methods=['GET', 'POST'])
+def upload():
+    try:
+        datasets = DataSet.query.all()
+        if request.method == 'POST':
+            names = []
+            files = request.files.getlist("file")
+            for file in files: 
+                file.save(os.path.join(app.config['UPLOAD_FOLDER'], file.filename))
+                names.append(file.filename)
             for name in names:
                 dataset = DataSet(name=name)
                 if not db.session.query(db.session.query(DataSet).filter_by(name=name).exists()).scalar():
                     db.session.add(dataset)
                     db.session.commit()
-            return render_template('success.html', files=files, datasets=datasets)
-        except (KeyboardInterrupt):
-            return 'Ошибка'
-    else:
-        return render_template('index.html')
-    
-#     # session['uploaded_files'] = ''
-#     # if request.method == 'POST':
-#     #     files = request.files.getlist("file")
-#     #     for file in files:
-#     #         file.save(os.path.join(app.config['UPLOAD_FOLDER'], file.filename))
-#     #         session['uploaded_files'] += os.path.join(app.config['UPLOAD_FOLDER'], file.filename+' ')
-#     #     return render_template('success.html', files=files)
-    #return render_template("index.html")
- 
-@app.route('/show/<int:id>')
+            datasets = DataSet.query.all()
+            return render_template('success.html', datasets=datasets)
+    except:
+        return render_template('upload.html', datasets=datasets)    
+    else:   
+        return render_template('upload.html', datasets=datasets)
+
+#--Отображение csv в виде таблиц на html
+@app.route('/upload/<int:id>')
 def show(id):
     dataset = DataSet.query.get(id)
     file_name = (DataSet.query.filter_by(id=dataset.id).first().name)
@@ -60,6 +69,7 @@ def show(id):
     file_pd = pd.read_csv(file, error_bad_lines=False, engine="python", encoding='unicode_escape')
     file_to_html = file_pd.to_html()
     return render_template('read.html', file_to_html=file_to_html)
+
 
 if __name__ == '__main__':
     app.run(debug=True)
